77e586595f19fa4a1171e1e516f5138470d36b2c
Damian Dennis <damiandennis@gmail.com>
2014-05-16 22:39:19 +0000

    added widgets to load.

---
+++ src/widgets/ScrollingBlocks.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * User: Damian
+ * Date: 17/05/14
+ * Time: 3:45 AM
+ */
+
+namespace damiandennis\scrollingblocks;
+
+use Yii;
+use yii\base\Widget;
+
+class ScrollingBlocks extends Widget
+{
+    public function run()
+    {
+        echo "hello";
+        parent::run();
+    }
+}
